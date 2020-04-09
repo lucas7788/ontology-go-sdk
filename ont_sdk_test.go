cbfbf6e9533c5061f66dd1f752a6babbf25e43bf
lucas7788 <sishsh@163.com>
2020-04-09 16:59:21 +0800

    add claim support

--- ont_sdk_test.go
+++ ont_sdk_test.go
@@ -48,11 +48,7 @@ var (
 
 func init() {
 	var err error
-	testWallet, err = testOntSdk.OpenWallet("./wallet.dat")
-	if err != nil {
-		fmt.Printf("OpenWallet err: %s\n", err)
-		return
-	}
+	testWallet = NewWallet("./wallet.dat")
 	testOntSdk = NewOntologySdk()
 	testOntSdk.NewRpcClient().SetAddress("http://127.0.0.1:20336")
 	testDefAcc, err = testWallet.GetDefaultAccount(testPasswd)
